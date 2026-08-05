7acd78f617e868828c2a4fbd055ec766d9a99e00
SHREEYA MUKHERJEE <rsshreeyamukherjee17@gmail.com>
2026-08-05 23:36:55 +0530

    Create linked-list.c

---
+++ linked-list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* reverseList(struct Node* head) {
+    struct Node* prev = NULL;
+    struct Node* current = head;
+    struct Node* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;  
+        current->next = prev;  
+        prev = current;        
+        current = next;        
+    }
+    return prev; 
+}
